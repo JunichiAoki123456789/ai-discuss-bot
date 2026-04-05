a0dad24ea1e6cf99feaf4e7d39427053f0cc0efb
JunichiAoki123456789 <49689745+JunichiAoki123456789@users.noreply.github.com>
2026-04-05 09:41:27 +0900

    Update bottest.py to print Hello World

--- bottest.py
+++ bottest.py
@@ -1,4 +1,4 @@
-def test_call_claude():
-    from bot import call_claude
-    response = call_claude("Hello, how are you?")
-    assert "Hello" in response
+def hello_world():
+    print("Hello, World!")
+
+hello_world()
