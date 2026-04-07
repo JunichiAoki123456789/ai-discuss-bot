1867390bb1b5f4887b8e9b4a659f939986622a2e
JunichiAoki123456789 <49689745+JunichiAoki123456789@users.noreply.github.com>
2026-04-07 21:59:08 +0900

    feat: !hello, !time, !help コマンドとエラーハンドリングを追加

--- bot_test.py
+++ bot_test.py
@@ -1,13 +1,20 @@
 import discord
 import os
+import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
+if TOKEN == None:
+    print("Error: DISCORD_TOKEN is not set")
+    exit()
+
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
+
+COMMANDS = ["!ping", "!hello", "!time", "!help"]
 
 @client.event
 async def on_ready():
@@ -17,7 +24,18 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+
     if message.content == "!ping":
         await message.channel.send("🏓 Pong! Bot is alive.")
+
+    if message.content == "!hello":
+        await message.channel.send("こんにちは！" + message.author.name + "さん")
+
+    if message.content == "!time":
+        now = datetime.datetime.now()
+        await message.channel.send("現在時刻: " + str(now))
+
+    if message.content == "!help":
+        await message.channel.send("使えるコマンド: !ping, !hello, !time, !help")
 
 client.run(TOKEN)
